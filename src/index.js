e7678e4fa75579aa3f0179b07b59b35bad0ba783
davidjj76 <davidjj76@gmail.com>
2022-05-10 21:34:59 +0200

    Hoc 2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App color="red" data={[]} />
+    <App color="red" />
   </React.StrictMode>,
 );
 
--- src/withFetch.js
+++ src/withFetch.js
@@ -3,13 +3,14 @@ import useFetch from './useFetch';
 
 export default function withFetch(url) {
   return function (WrappedComponent) {
-    return function WithFetchComponent(props) {
+    const WithFetchComponent = props => {
       const { isLoading, error, data } = useFetch(url);
 
       if (isLoading) return 'Loading...';
       if (error) return `Meeehh, error: ${error.message}`;
       return <WrappedComponent data={data} {...props} />;
     };
+    return WithFetchComponent;
     // return class WithFetchComponent extends React.Component {};
   };
 }
