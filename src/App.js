9fd9da786697fb5ba1d7e5d94ad64765608a11b5
davidjj76 <davidjj76@gmail.com>
2022-05-10 20:31:59 +0200

    Usereducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 
-const TEAMS_URL = 'https://www.balldontlie.io/api/v1/teams';
+const TEAMS_URL = 'https://www.balldontlie.io/api/v1/te';
+
+const initialState = {
+  isLoading: false,
+  teams: [],
+  error: null,
+};
+
+const TEAMS_FETCH_REQUEST = 'TEAMS_FETCH_REQUEST';
+const TEAMS_FETCH_SUCCESS = 'TEAMS_FETCH_SUCCESS';
+const TEAMS_FETCH_FAILURE = 'TEAMS_FETCH_FAILURE';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case TEAMS_FETCH_REQUEST:
+      return { ...state, isLoading: true, error: null };
+    case TEAMS_FETCH_SUCCESS:
+      return { ...state, isLoading: false, teams: action.payload };
+    case TEAMS_FETCH_FAILURE:
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+const fetchTeamsRequest = () => ({
+  type: TEAMS_FETCH_REQUEST,
+});
+
+const fetchTeamsSuccess = teams => ({
+  type: TEAMS_FETCH_SUCCESS,
+  payload: teams,
+});
+
+const fetchTeamsFailure = error => ({
+  type: TEAMS_FETCH_FAILURE,
+  payload: error,
+  error: true,
+});
 
 function App() {
-  const [teams, setTeams] = useState([]);
+  const [{ isLoading, teams, error }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   useEffect(() => {
+    dispatch(fetchTeamsRequest());
     fetch(TEAMS_URL)
       .then(response => response.json())
-      .then(({ data }) => setTeams(data));
+      .then(({ data }) => dispatch(fetchTeamsSuccess(data)))
+      .catch(error => dispatch(fetchTeamsFailure(error)));
   }, []);
+
+  if (isLoading) return 'Loading...';
+  if (error) return `Meeehh, error: ${error.message}`;
 
   return (
     <div className="App">
@@ -18,7 +64,7 @@ function App() {
       {teams.length > 0 && (
         <ul>
           {teams.map(team => (
-            <li>{team.full_name}</li>
+            <li key={team.id}>{team.full_name}</li>
           ))}
         </ul>
       )}
